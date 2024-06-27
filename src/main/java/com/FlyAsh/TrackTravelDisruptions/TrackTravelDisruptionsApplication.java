3f5f0c7069aee7ae1892887a9fbbe5614a9978c8
SunLawnY <sunnylaw3571@gmail.com>
2024-06-27 12:35:10 +0100

    first commit

---
+++ src/main/java/com/FlyAsh/TrackTravelDisruptions/TrackTravelDisruptionsApplication.java
@@ -0,0 +1,13 @@
+package com.FlyAsh.TrackTravelDisruptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackTravelDisruptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackTravelDisruptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FlyAsh/TrackTravelDisruptions/TrackTravelDisruptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FlyAsh.TrackTravelDisruptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackTravelDisruptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
